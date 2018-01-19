2b187f0d3ca008f4a77c66ceb47987596e43943a
sdosapati <sdosapati@indeed.com>
2018-01-19 13:03:39 -0800

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,10 @@
+import java.net.InetAddress;
 
 public class HelloWorld {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		System.out.println("God is great..");
-		
+		InetAddress ia = InetAddress.getLocalHost();
+		System.out.println(ia);
+		System.out.println("Trying something else..");
 	}
 }
