68313a6e621454bf345ab51f29e81bcfebcd750e
annlearner <31709240+annlearner@users.noreply.github.com>
2018-03-21 19:11:55 -0700

    Update Dealer.java

--- Assignments/ProjectOne/Dealer.java
+++ Assignments/ProjectOne/Dealer.java
@@ -1,11 +1,17 @@
 package com.company;
 
-public class Dealer extends Player {
+public class Dealer {
 
-    GroupOfCards memoryCard;
+    Hand hand;
 
-    @Override
-    public boolean draw(Card dealer, Hand hand) {
+    Dealer(){
+   
+    };
+
+    public boolean draw(){
         return false;
     }
+    // EFFECTS: For dealer The dealer must
+    // hit until he either reaches a total greater than or equal to 17
+    // (hard or soft), or busts.
 }
