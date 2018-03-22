e3683695c9af7f8cce8c567f9ddbf9a53573294c
annlearner <31709240+annlearner@users.noreply.github.com>
2018-03-21 19:12:56 -0700

    Update CountingPlayer.java

--- Assignments/ProjectOne/CountingPlayer.java
+++ Assignments/ProjectOne/CountingPlayer.java
@@ -2,9 +2,31 @@ package com.company;
 
 public class CountingPlayer extends Player {
 
+    int memoryCard;
+
+    CountingPlayer(int bankroll, int minimum){
+        super(bankroll, minimum);
+        memoryCard = 0;
+    }
 
     @Override
-    public boolean draw(Card dealer, Hand hand) {
+    public int bet() {
+        return 0;
+    }
+
+    @Override
+    public boolean draw(Card dealer) {
         return false;
     }
+
+    @Override
+    public void expose(Card c) {
+
+    }
+
+    @Override
+    public void shuffled() {
+
+    }
 }
+
