c4b0c1740c3d77425517efd0b3b823f48ceb8e45
annlearner <31709240+annlearner@users.noreply.github.com>
2018-03-21 19:14:28 -0700

    Update Spot.java

--- Assignments/ProjectOne/Spot.java
+++ Assignments/ProjectOne/Spot.java
@@ -2,7 +2,17 @@ package com.company;
 
 public enum Spot {
     TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
-    JACK, QUEEN, KING, ACE
+    JACK, QUEEN, KING, ACE;
+
+    public int getValue(){
+        if(this.ordinal() <= 8){
+            return this.ordinal() + 2;
+        }else if(this.ordinal() <= 11) {
+            return 10;
+        }else{
+            return 1;
+        }
+    }
 }
 
 // for (Spot e : Spot.values()) {
