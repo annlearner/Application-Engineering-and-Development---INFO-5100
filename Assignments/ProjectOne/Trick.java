73e79c905b514a438a6d4fe9d2fa93fa027728bf
annlearner <31709240+annlearner@users.noreply.github.com>
2018-03-21 19:15:17 -0700

    Update Trick.java

--- Assignments/ProjectOne/Trick.java
+++ Assignments/ProjectOne/Trick.java
@@ -1,7 +1,35 @@
 package com.company;
 
 public class Trick {
-    // one player's turn + one dealer's turn. But dealer's turn
-    // only happens if the player hasn’t busted or the player is
+
+    Player player;
+    Dealer dealer;
+    int bet;
+
+    Trick(String type, int bankroll, int minimum){};
+
+    void play(){
+
+    }
+    // execute the five following steps;
+
+    void wager(){
+    };
+
+    void DealFour(){};
+    //After the wager, the dealer deals a total of four cards:
+    // the first face-up to the player, the second face-up to
+    // himself, the third face-up to the player, and the fourth
+    // face-down to himself.
+
+    void playerTurn(){};
+
+    void dealerTurn(){};
+
+    // dealer's turn only happens if the player hasn’t busted or the player is
     // not dealt a natural 21 .
+
+    void closeTurn(){};
+    // compare the count and give back reward to player if win.
+
 }
